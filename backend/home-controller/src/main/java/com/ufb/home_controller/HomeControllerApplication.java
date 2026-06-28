5946701a86cc47d48f5af9a40edc4ad216dfa2cc
ngabonzizacedrickkennedy <cedzlankee42@gmail.com>
2026-06-28 20:51:23 +0200

    ch

---
+++ backend/home-controller/src/main/java/com/ufb/home_controller/HomeControllerApplication.java
@@ -0,0 +1,13 @@
+package com.ufb.home_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeControllerApplication.class, args);
+	}
+
+}
---
+++ backend/home-controller/src/test/java/com/ufb/home_controller/HomeControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufb.home_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
